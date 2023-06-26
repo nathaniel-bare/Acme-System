d531c8347f71995cd7765233f1ce054bb4eb9fbb
nathaniel-bare <ndbare@valdosta.edu>
2023-06-25 23:49:37 -0400

    Added the heyNow method to HelloWorld

--- src/main/java/ver1/HelloDolly.java
+++ src/main/java/ver1/HelloDolly.java
@@ -1,7 +1,8 @@
 package ver1;
 
 public class HelloDolly {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Person 2 is at work!");
     }
+
 }
